2da8fff6c8e887db016af5c33b6379a911951676
Theofilos Intzoglou <int.teo@gmail.com>
2022-07-20 23:32:37 +0300

    Exercise 8

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import useGlobalEvent from "@/composables/useGlobalEvent";
 
 const props = defineProps<{
 	src: string,
 }>();
 
-const video = ref(null); 
+const video = ref(null);
+
+function checkKeyPressed(event: KeyboardEvent) {
+	if (event.charCode === 32) {
+		event.preventDefault();
+		video.value.paused ? video.value.play() : video.value.pause()
+	}
+}
+
+useGlobalEvent("keypress", checkKeyPressed);
 </script>
 <template>
 	<div>
