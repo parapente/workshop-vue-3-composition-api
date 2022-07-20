b9815d3ce9649864cf2beeb2ea371c0493d4293b
Theofilos Intzoglou <int.teo@gmail.com>
2022-07-20 23:23:24 +0300

    Exercise 7

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Rick & Morty Characters" />
+    <!-- <HelloWorld msg="Rick & Morty Characters" /> -->
+    <VideoPlayer src="/Dog on the beach.mp4" />
   </div>
 </template>
 
 <script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import VideoPlayer from "./components/VideoPlayer.vue";
 </script>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps<{
+	src: string,
+}>();
+
+const video = ref(null); 
+</script>
+<template>
+	<div>
+		<video :src="props.src" ref="video" />
+		<div class="video-buttons" v-if="video">
+			<button @click="video.play()">Play</button>
+			<button @click="video.pause()">Pause</button>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+video {
+	width: 800px;
+	margin: 0 auto;
+}
+
+.video-buttons {
+	width: 800px;
+	margin: 0 auto;
+}
+
+.video-buttons button {
+	padding: 5px;
+	border: 1px solid #000;
+	background: linear-gradient(to bottom, rgb(85, 245, 170), #fff)
+}
+</style>
